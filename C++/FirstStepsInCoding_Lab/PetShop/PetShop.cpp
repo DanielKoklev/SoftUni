69b76afa98c6b13ee5d813dbfc2645a140fe2150
dkoklev <danikoklef@gmail.com>
2022-01-08 18:21:44 +0200

    New exercises from Programming Basics with C++

---
+++ C++/FirstStepsInCoding_Lab/PetShop/PetShop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double dogFood;
+    cin >> dogFood;
+    dogFood *= 2.50;
+    
+    double catFood;
+    cin >> catFood;
+    catFood *= 4;
+
+    cout << catFood + dogFood;
+
+}
