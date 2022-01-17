8b975f938809beacd52dd15f334d0d49a012802e
dkoklev <danikoklef@gmail.com>
2022-01-17 23:34:51 +0200

    New exercise

---
+++ Fundamentals/Basic_syntax_conditional_statements_and_loops_exercise/Leonardo_Dicaprio_Oscars.py
@@ -0,0 +1,10 @@
+score = int(input())
+
+if score == 88:
+    print("Leo finally won the Oscar! Leo is happy")
+elif score == 86:
+    print("Not even for Wolf of Wall Street?!")
+elif score < 88 and score != 86:
+    print("When will you give Leo an Oscar?")
+elif score > 88:
+    print("Leo got one already!")
