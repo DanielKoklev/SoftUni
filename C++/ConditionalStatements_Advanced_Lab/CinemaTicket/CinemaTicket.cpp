0160f5066d38829c822d34bc95bc0d3b56740502
dkoklev <danikoklef@gmail.com>
2022-01-24 20:44:07 +0200

    New exercise

---
+++ C++/ConditionalStatements_Advanced_Lab/CinemaTicket/CinemaTicket.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string day;
+	cin >> day;
+
+	if (day == "Monday") {
+		cout << "12" << endl;
+	}
+	else if (day == "Tuesday") {
+		cout << "12" << endl;
+	}
+	else if (day == "Wednesday") {
+		cout << "14" << endl;
+	}
+	else if (day == "Thursday") {
+		cout << "14" << endl;
+	}
+	else if (day == "Friday") {
+		cout << "12" << endl;
+	}
+	else if (day == "Saturday") {
+		cout << "16" << endl;
+	}
+	else if (day == "Sunday") {
+		cout << "16" << endl;
+	}
+}
