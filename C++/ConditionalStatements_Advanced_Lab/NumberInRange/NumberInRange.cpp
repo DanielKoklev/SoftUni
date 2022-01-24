78eabe619ee54ce4881b0a76418626e51ff13190
dkoklev <danikoklef@gmail.com>
2022-01-24 20:45:08 +0200

    New exercise

---
+++ C++/ConditionalStatements_Advanced_Lab/NumberInRange/NumberInRange.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	if (n >= -100 && n <= 100 && n != 0) {
+		cout << "Yes" << endl;
+	}
+	else {
+		cout << "No" << endl;
+	}
+
+}
