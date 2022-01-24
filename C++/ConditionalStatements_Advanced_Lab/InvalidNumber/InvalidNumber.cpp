0f893dad0ff7c2ec790485ca65d5bcf5f860c813
dkoklev <danikoklef@gmail.com>
2022-01-24 20:44:59 +0200

    New exercise

---
+++ C++/ConditionalStatements_Advanced_Lab/InvalidNumber/InvalidNumber.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	if (n >= 100 && n <= 200 || n == 0) {
+	}
+	else {
+		cout << "invalid" << endl;
+	}
+	return 0;
+
+}
