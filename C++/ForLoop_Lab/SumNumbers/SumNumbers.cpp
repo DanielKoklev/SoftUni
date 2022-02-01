92f7b77c3baec6c2a5dd8b7d0788170818981dd8
dkoklev <danikoklef@gmail.com>
2022-02-01 13:06:39 +0200

    New exercise

---
+++ C++/ForLoop_Lab/SumNumbers/SumNumbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int sum = 0;
+	int num = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> num;
+		sum += num;
+	}
+	cout << sum;
+}
