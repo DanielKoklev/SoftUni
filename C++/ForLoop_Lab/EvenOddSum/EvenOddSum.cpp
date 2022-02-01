c60b9aedfa951e58f8ee88665b6c6a34aed25b63
dkoklev <danikoklef@gmail.com>
2022-02-01 13:05:13 +0200

    New exercise

---
+++ C++/ForLoop_Lab/EvenOddSum/EvenOddSum.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int odd = 0;
+	int even = 0;
+
+	for (int i = 1; i <= n; i++)
+	{
+		int num;
+		cin >> num;
+
+		if (i % 2 == 0) {
+			even += num;
+		}
+		else {
+			odd += num;
+		}
+	}
+
+	int diff = abs(odd - even);
+	if (diff == 0) {
+		cout << "Yes" << endl;
+		cout << "Sum = " << odd << endl;
+	}
+	else {
+		cout << "No" << endl;
+		cout << "Diff = " << diff << endl;
+	}
+
+	return 0;
+}
