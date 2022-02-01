6bcadfc63cf30c7979bfdb4ab9fafb518bdcdec6
dkoklev <danikoklef@gmail.com>
2022-02-01 13:05:41 +0200

    New exercise

---
+++ C++/ForLoop_Lab/LeftRightSum/LeftRightSum.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int leftSum = 0;
+	int rightSum = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		int num;
+		cin >> num;
+		leftSum += num;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		int num;
+		cin >> num;
+		rightSum += num;
+	}
+
+	if (leftSum == rightSum) {
+		cout << "Yes, sum = " << leftSum << endl;
+	}
+	else {
+		cout << "No, diff = " << (abs(leftSum - rightSum)) << endl;
+	}
+}
