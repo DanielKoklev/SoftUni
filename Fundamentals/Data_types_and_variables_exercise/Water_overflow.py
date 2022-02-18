038ba6b92e26881da8718596529dd267a6d86520
dkoklev <danikoklef@gmail.com>
2022-02-18 18:53:08 +0200

    New exercise

---
+++ Fundamentals/Data_types_and_variables_exercise/Water_overflow.py
@@ -0,0 +1,14 @@
+n_of_inputs = int(input())
+
+capacity = 255
+litters_in = 0
+
+for _ in range(n_of_inputs):
+    quantity = int(input())
+    if quantity > capacity:
+        print("Insufficient capacity!")
+        continue
+    capacity -= quantity
+    litters_in += quantity
+
+print(litters_in)
