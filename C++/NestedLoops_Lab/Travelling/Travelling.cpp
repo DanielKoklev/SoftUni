cace2333581df167607c1051d5e1090363f3f0e6
dkoklev <danikoklef@gmail.com>
2022-02-18 18:47:47 +0200

    New exercise

---
+++ C++/NestedLoops_Lab/Travelling/Travelling.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string destination;
+	cin >> destination;
+
+	while (destination != "End")
+	{
+		double needBudget;
+		cin >> needBudget;
+
+		double savedMoney = 0;
+
+		while (savedMoney < needBudget)
+		{
+			double sum; cin >> sum;
+			savedMoney += sum;
+		}
+
+		cout << "Going to " << destination << "!" << endl;
+
+		cin >> destination;
+	}
+	return 0;
+}
