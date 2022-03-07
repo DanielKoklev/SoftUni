ab8d10d025cdda92107ec4b4009491af5e61d0b5
dkoklev <danikoklef@gmail.com>
2022-03-07 14:44:59 +0200

    New exercises

---
+++ Fundamentals/Dictionaries_Lab/Stock.py
@@ -0,0 +1,15 @@
+data = input().split(" ")
+bakery = dict()
+
+for i in range(0, len(data), 2):
+    key = data[i]
+    value = data[i+1]
+    bakery[key] = int(value)
+
+searched_products = input().split(" ")
+
+for product in searched_products:
+    if product in bakery:
+        print(f"We have {bakery[product]} of {product} left")
+    else:
+        print(f"Sorry, we don't have {product}")
